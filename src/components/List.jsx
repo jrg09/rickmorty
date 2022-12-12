1cfb89b5cfd2d477dbcbf465530887f3178d5af4
Jorge Castro <jorge.files@gmail.com>
2022-12-12 09:57:21 -0600

    add setIsLoading

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from "react";
-import { Card, Button } from "react-bootstrap";
 import { Character } from "./Character";
 
 export const List = () => {
-  const items = ["uno", "dos", "tres"];
-
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchAPI = async () => {
+    const fetchData = async () => {
       const data = await fetch("https://rickandmortyapi.com/api/character");
       const { results } = await data.json();
       setCharacters(results);
+      setIsLoading(false);
     };
 
-    fetchAPI();
+    fetchData();
   }, [characters.length]);
 
   return (
     <>
       <div className="row mt-3">
-        {characters.map((character) => (
-          <Character key={character.id} character={character} />
-        ))}
+        {isLoading ? (
+          <div>Cargando...</div>
+        ) : (
+          characters.map((character) => (
+            <Character key={character.id} character={character} />
+          ))
+        )}
       </div>
     </>
   );
